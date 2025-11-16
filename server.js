7e7e724868b155517ef2a757e7d66759a943c338
cyrinesaidali <cyrineSA@hotmail.com>
2025-11-16 17:05:58 +0100

    Test

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const cors = require('cors'); // Import du middleware CORS
 // Création de la constante App → Va permettre de faire tourner le serveur +  permet d'envoyer les différentes pages HTML
 const app = express();
 
-//DIFFÉRENTES ROUTES POUR L'APP : Mise en place des différentes routes avec les pages 
+//DIFFÉRENTES ROUTES POUR L'APP : Mise en place des différentes routes avec les pages Tetststststststs 
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public/pages')));
